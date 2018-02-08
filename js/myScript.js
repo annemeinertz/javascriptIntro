05e5cf7b5cbee579b33bbca3b6d678eb5422e455
Unknown <annemeinertz@live.dk>
2018-02-08 14:00:52 +0100

    Slutning undervisning

--- js/myScript.js
+++ js/myScript.js
@@ -5,6 +5,10 @@
  **/
 
 // variabler
+var vand = "vand"; // teksttreng
+var vandIgen ='vand';
+var vandAahNej ='vand smager da "rigtig godt"';
+
 var tal = 111;
 var erDetRigtigt = false;
 var enStreng = "hej verden";
@@ -12,6 +16,7 @@ var enStreng = "hej verden";
 // klister (+)
 // kommentarer
 // regning
+var talMere = 999;
 // datoer
 // lister (scratch: data)
 // hvis ... så ... (scratch: operators)
@@ -21,9 +26,9 @@ var enStreng = "hej verden";
 // noget her
 
 /* se resultatet af din kode i browserens udviklings-værktøj */
-console.log( enStreng  );
+console.log( vandAahNej );
 // skriv i browservinduet
-
+document.write ("<h1>" + "Mojn, mojn do!" + "<h1>");
 /* se resultatet af din kode, der hvor scriptet ligger i koden */
 
 // næste gang: events.
